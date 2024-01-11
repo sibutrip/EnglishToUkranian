41c1f14d9fe4bb80d64e9a9d816fb1c6dda20bde
Cory Sibu Tripathy <109297828+sibutrip@users.noreply.github.com>
2024-01-11 09:45:13 -0500

    Initial Commit

---
+++ UkranianToEnglish/UkranianToEnglishApp.swift
@@ -0,0 +1,17 @@
+//
+//  UkranianToEnglishApp.swift
+//  UkranianToEnglish
+//
+//  Created by Cory Tripathy on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct UkranianToEnglishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
